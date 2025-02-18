9804f75396135bcd4226ddf9558056cea9519f1a
Ankan5960 <ankanmaity27@gmail.com>
2025-02-19 00:39:39 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-black text-white'>
+        <h1>hello</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
