0cf61f80a6ee8af7f551eb8a5380b5fdf94bf2c5
Ankan5960 <ankanmaity27@gmail.com>
2025-02-21 23:46:52 +0530

    format change

--- src/components/Tree/tree.tsx
+++ src/components/Tree/tree.tsx
@@ -1,59 +1,62 @@
 import { useEffect, useState } from "react";
 
 const Tree: React.FC = () => {
-    const [data, setData] = useState<{ slidebar: any[] } | null>(null);
-    const [expandedItems, setExpandedItems] = useState<{ [key: number]: boolean }>({});
+  const [data, setData] = useState<{ slidebar: any[] } | null>(null);
+  const [expandedItems, setExpandedItems] = useState<{
+    [key: number]: boolean;
+  }>({});
 
-    useEffect(() => {
-        fetch("/Data/slidebarData.json")
-            .then((res) => res.json())
-            .then((Data) => {
-                setData(Data);
-                console.log(Data);
-            })
-            .catch((error) => console.log(error));
-    }, []);
+  useEffect(() => {
+    fetch("/Data/slidebarData.json")
+      .then((res) => res.json())
+      .then((Data) => {
+        setData(Data);
+        console.log(Data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-    const toggleExpand = (index: number) => {
-        setExpandedItems((prev) => ({
-            ...prev,
-            [index]: !prev[index],
-        }));
-    };
+  const toggleExpand = (index: number) => {
+    setExpandedItems((prev) => ({
+      ...prev,
+      [index]: !prev[index],
+    }));
+  };
 
-    return (
-        <div className="p-2">
-            {data ? (
-                <ul>
-                    {data.slidebar.map((item, index) => (
-                        <li className="flex flex-col hover:bg-slate-600" key={index} >
-                            <div onClick={() => toggleExpand(index)} className="flex justify-between items-center">
-                                <a >{item.name}</a>
-                                {item.Children.length > 0 && (
-                                    <button
-                                        className="ml-2 text-white w-6 h-6 rounded-md flex items-center justify-center"
-                                    >
-                                        {expandedItems[index] ? "-" : "+"}
-                                    </button>
-                                )}
-                            </div>
-                            {item.Children.length > 0 && expandedItems[index] && (
-                                <ul className="flex-row" >
-                                    {item.Children.map((child: any, childIndex: number) => (
-                                        <li className=" hover:bg-slate-700 pl-4" key={childIndex}>
-                                            {child.name}
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </li>
-                    ))}
+  return (
+    <div className="p-2">
+      {data ? (
+        <ul>
+          {data.slidebar.map((item, index) => (
+            <li className="flex flex-col hover:bg-slate-600" key={index}>
+              <div
+                onClick={() => toggleExpand(index)}
+                className="flex justify-between items-center"
+              >
+                <a>{item.name}</a>
+                {item.Children.length > 0 && (
+                  <button className="ml-2 text-white w-6 h-6 rounded-md flex items-center justify-center">
+                    {expandedItems[index] ? "-" : "+"}
+                  </button>
+                )}
+              </div>
+              {item.Children.length > 0 && expandedItems[index] && (
+                <ul className="flex-row">
+                  {item.Children.map((child: any, childIndex: number) => (
+                    <li className=" hover:bg-slate-700 pl-4" key={childIndex}>
+                      {child.name}
+                    </li>
+                  ))}
                 </ul>
-            ) : (
-                <p>Loading...</p>
-            )}
-        </div>
-    );
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default Tree;
--- src/pages/sidebar.tsx
+++ src/pages/sidebar.tsx
@@ -3,11 +3,6 @@ import Tree from "../components/Tree/tree";
 const SideBar: React.FC = () => {
   return (
     <div className="h-full overflow-auto bg-slate-850">
-      {/* {Array.from({ length: 20 }, (_, i) => (
-        <div key={i} className="p-2 mx-4 rounded-lg my-4 bg-slate-700">
-          I am good
-        </div>
-      ))} */}
       <Tree />
     </div>
   );
